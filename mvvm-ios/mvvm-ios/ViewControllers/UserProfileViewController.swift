c78cbc15082f8d6e1042d47d09217a08968526e8
Sergii Nezdolii <sergii@frost.se>
2017-11-23 21:36:44 +0100

    [ADD] adding User Profile viewcontroller, glueing with storyboard

---
+++ mvvm-ios/mvvm-ios/ViewControllers/UserProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  UserProfileViewController.swift
+//  mvvm-ios
+//
+//  Created by Sergii Frost on 2017-11-23.
+//  Copyright © 2017 Frost Experience AB. All rights reserved.
+//
+
+import UIKit
+
+public class UserProfileViewController: UIViewController {
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var bioTextView: UITextView!
+    @IBOutlet weak var createdContainerView: UIView!
+    @IBOutlet weak var createdLabel: UILabel!
+    @IBOutlet weak var organisationContainerView: UIView!
+    @IBOutlet weak var organisationLabel: UILabel!
+    @IBOutlet weak var locationContainerView: UIView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var emailContainerView: UIView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var linkView: UIView!
+    @IBOutlet weak var linkLabel: UILabel!
+    @IBOutlet weak var reposContainerView: UIView!
+    @IBOutlet weak var reposLabel: UILabel!
+    @IBOutlet weak var gistsContainerView: UIView!
+    @IBOutlet weak var gistsLabel: UILabel!
+    
+}
