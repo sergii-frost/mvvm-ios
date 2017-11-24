88d7b3f0b1ce059a05bcc0558bd8ea0b404d549f
Sergii Nezdolii <sergii@frost.se>
2017-11-24 01:05:17 +0100

    [DEV] making viewmodel observable via RxSwift

--- mvvm-ios/mvvm-ios/ViewModels/UserProfileViewModel.swift
+++ mvvm-ios/mvvm-ios/ViewModels/UserProfileViewModel.swift
@@ -7,57 +7,58 @@
 //
 
 import Foundation
+import RxSwift
 
 public class UserProfileViewModel {
-    var username: String
+    var username: String?
     
-    var login: String = ""
-    var avatarUrl: String = ""
-    var name: String?
-    var company: String?
-    var location: String?
-    var email: String?
-    var bio: String?
-    var blog: String?
-    var followers: Int = 0
-    var following: Int = 0
-    var createdAt: Date = Date()
-    var publicRepos: Int = 0
-    var publicGists: Int = 0
-    var reposUrl: String = ""
-    var gistsUrl: String = ""
+    let login = Variable<String>("")
+    var avatarUrl = Variable<String>("")
+    var name = Variable<String?>(nil)
+    var company = Variable<String?>(nil)
+    var location = Variable<String?>(nil)
+    var email = Variable<String?>(nil)
+    var bio = Variable<String?>(nil)
+    var blog = Variable<String?>(nil)
+    var followers = Variable<Int>(0)
+    var following = Variable<Int>(0)
+    var createdAt = Variable<Date>(Date())
+    var publicRepos = Variable<Int>(0)
+    var publicGists = Variable<Int>(0)
+    var reposUrl = Variable<String>("")
+    var gistsUrl = Variable<String>("")
     
-    var message: String?
-    var statusCode: Int = 0
+    var message = Variable<String?>(nil)
+    var statusCode = Variable<Int>(0)
     
-    public init(username: String) {
+    public init(username: String?) {
         self.username = username
     }
     
     public func fetchProfile() {
-        GithubService.shared.getUserProfile(with: username, success: { [weak self] user in
+        GithubService.shared.getUserProfile(with: username ?? "", success: { [weak self] user in
             self?.update(withUser: user)
         }, failure: { [weak self] message, statusCode in
-            self?.message = message
-            self?.statusCode = statusCode
+            self?.message.value = message
+            self?.statusCode.value = statusCode
         })
     }
     
     fileprivate func update(withUser user: GHUser) {
-        login = user.login
-        avatarUrl = user.avatarUrl
-        name = user.name
-        company = user.company
-        location = user.location
-        email = user.email
-        bio = user.bio
-        blog = user.blog
-        followers = user.followers
-        following = user.following
-        createdAt = user.createdAt
-        publicRepos = user.publicRepos
-        publicGists = user.publicGists
-        reposUrl = user.reposUrl
-        gistsUrl = user.gistsUrl
+        login.value = user.login
+        avatarUrl.value = user.avatarUrl
+        name.value = user.name
+        company.value = user.company
+        location.value = user.location
+        email.value = user.email
+        bio.value = user.bio
+        blog.value = user.blog
+        followers.value = user.followers
+        following.value = user.following
+        createdAt.value = user.createdAt
+        publicRepos.value = user.publicRepos
+        publicGists.value = user.publicGists
+        reposUrl.value = user.reposUrl
+        gistsUrl.value = user.gistsUrl
     }
 }
