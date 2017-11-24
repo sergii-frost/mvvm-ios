3a35503c2b56bb3ca3d85cb191df1bd6e42e7682
Sergii Nezdolii <sergii@frost.se>
2017-11-24 01:05:45 +0100

    [DEV] setting up viewmodel bindings

--- mvvm-ios/mvvm-ios/ViewControllers/UserProfileViewController.swift
+++ mvvm-ios/mvvm-ios/ViewControllers/UserProfileViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Kingfisher
+import RxSwift
+import RxCocoa
 
 public class UserProfileViewController: UIViewController {
     
@@ -29,6 +32,13 @@ public class UserProfileViewController: UIViewController {
     @IBOutlet weak var gistsLabel: UILabel!
     
     var viewModel: UserProfileViewModel?
+    let disposeBag = DisposeBag()
+    
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM dd yyyy"
+        return dateFormatter
+    }()
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +50,94 @@ public class UserProfileViewController: UIViewController {
         guard let viewModel = viewModel else {
             return
         }
-        //TODO: setup bindings
+        viewModel.name.asObservable().bind(to: nameLabel.rx.text).disposed(by: disposeBag)
+        viewModel.login.asObservable().bind(to: loginLabel.rx.text).disposed(by: disposeBag)
+        
+        viewModel.avatarUrl.asObservable()
+            .map { avatarUrl -> URL? in
+                return URL(string: avatarUrl)
+            }.bind { [weak self] url in
+                self?.avatarImageView.kf.setImage(with: url)
+            }.disposed(by: disposeBag)
+        
+        viewModel.bio.asObservable().bind(to: bioTextView.rx.text).disposed(by: disposeBag)
+        viewModel.createdAt.asObservable()
+            .map { [weak self] date -> String? in
+                return self?.dateFormatter.string(from: date)
+            }.bind(to: createdLabel.rx.text)
+            .disposed(by: disposeBag)
+        viewModel.company.asObservable().bind(to: companyLabel.rx.text).disposed(by: disposeBag)
+        viewModel.location.asObservable().bind(to: locationLabel.rx.text).disposed(by: disposeBag)
+        viewModel.email.asObservable().bind(to: emailLabel.rx.text).disposed(by: disposeBag)
+        viewModel.blog.asObservable().bind(to: linkLabel.rx.text).disposed(by: disposeBag)
+        viewModel.publicRepos.asObservable()
+            .map { repos -> String in
+                return "Public Repos #: \(repos)"
+            }.bind(to: reposLabel.rx.text)
+            .disposed(by: disposeBag)
+        viewModel.publicGists.asObservable()
+            .map { gists -> String in
+                return "Public Gists #: \(gists)"
+            }.bind(to: gistsLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        setupVisibilityBindings()
+    }
+    
+    fileprivate func setupVisibilityBindings() {
+        guard let viewModel = viewModel else {
+            return
+        }
+        //Binding hiding views - yeah, a bit tricky
+        viewModel.bio.asObservable()
+            .map { bio -> Bool in
+                return bio?.isEmpty ?? true
+            }.bind(onNext: { [weak self] isHidden in
+                self?.bioTextView.isHidden = isHidden
+            }).disposed(by: disposeBag)
+        
+        viewModel.company.asObservable()
+            .map { company -> Bool in
+                return company?.isEmpty ?? true
+            }.bind(onNext: { [weak self] isHidden in
+                self?.companyContainerView.isHidden = isHidden
+            }).disposed(by: disposeBag)
+        
+        viewModel.location.asObservable()
+            .map { location -> Bool in
+                return location?.isEmpty ?? true
+            }.bind(onNext: { [weak self] isHidden in
+                self?.locationContainerView.isHidden = isHidden
+            }).disposed(by: disposeBag)
+        
+        viewModel.email.asObservable()
+            .map { email -> Bool in
+                return email?.isEmpty ?? true
+            }.bind(onNext: { [weak self] isHidden in
+                self?.emailContainerView.isHidden = isHidden
+            }).disposed(by: disposeBag)
+        
+        viewModel.blog.asObservable()
+            .map { blog -> Bool in
+                return blog?.isEmpty  ?? true
+            }.bind(onNext: { [weak self] isHidden in
+                self?.linkContainerView.isHidden = isHidden
+            }).disposed(by: disposeBag)
+        
+        viewModel.publicRepos.asObservable()
+            .map { repos -> Bool in
+                return repos == 0
+            }.bind(onNext: { [weak self] isHidden in
+                self?.reposContainerView.isHidden = isHidden
+            }).disposed(by: disposeBag)
+        
+        viewModel.publicGists.asObservable()
+            .map { gists -> Bool in
+                return gists == 0
+            }.bind(onNext: { [weak self] isHidden in
+                self?.gistsContainerView.isHidden = isHidden
+            }).disposed(by: disposeBag)
+
     }
     
     //MARK: IBActions
